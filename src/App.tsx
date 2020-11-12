6e6efe9d5ca9de9dd70a1992977cc0b3b6a91796
Huong Pham <huong.pq12@gmail.com>
2020-11-12 10:04:47 +0200

    chore: add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 import React from "react";
 import "./styles/app.scss";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import SideNav from "./components/SideNav";
+
+import TodayPage from "./pages/Today";
+import WeekPage from "./pages/Week";
+import HabitPage from "./pages/Habit";
 
 function App() {
   return (
     <div className="App">
       <h1>DONE BY ME</h1>
+      <BrowserRouter>
+        <SideNav />
+        <Switch>
+          <Route exact path="/">
+            <TodayPage />
+          </Route>
+          <Route path="/week">
+            <WeekPage />
+          </Route>
+          <Route path="/habit">
+            <HabitPage />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
