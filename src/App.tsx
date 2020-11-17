b3c50e1c12b586f536a484dd3f64440cd194d467
Huong Pham <huong.pq12@gmail.com>
2020-11-17 11:16:44 +0200

    style: rearrange layout

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,36 @@ import SideNav from "./components/SideNav";
 import TodayPage from "./pages/Today";
 import WeekPage from "./pages/Week";
 import HabitPage from "./pages/Habit";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App() {
   return (
     <div className="App">
       <h1>DONE BY ME</h1>
-      <BrowserRouter>
-        <SideNav />
-        <Switch>
-          <Route exact path="/">
-            <TodayPage />
-          </Route>
-          <Route path="/week">
-            <WeekPage />
-          </Route>
-          <Route path="/habit">
-            <HabitPage />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      <Container fluid>
+        <BrowserRouter>
+          <Row>
+            <Col lg={2} md={3}>
+              <SideNav />
+            </Col>
+            <Col>
+              <Switch>
+                <Route exact path="/">
+                  <TodayPage />
+                </Route>
+                <Route path="/week">
+                  <WeekPage />
+                </Route>
+                <Route path="/habit">
+                  <HabitPage />
+                </Route>
+              </Switch>
+            </Col>
+          </Row>
+        </BrowserRouter>
+      </Container>
     </div>
   );
 }
