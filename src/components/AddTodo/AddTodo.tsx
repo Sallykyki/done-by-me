7e534231f2e5d21ebe5556ae5185bd6f8e82299f
Huong Pham <huong.pq12@gmail.com>
2020-12-03 15:54:00 +0200

    style: mdify add todo button

--- src/components/AddTodo/AddTodo.tsx
+++ src/components/AddTodo/AddTodo.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
+import { Plus } from "react-bootstrap-icons";
 
 interface IProps {
   onClick: () => void;
 }
 
 const AddTodo: React.FunctionComponent<IProps> = (props) => {
-  return <Button onClick={props.onClick}>Add todo</Button>;
+  return (
+    <Button
+      className="component-AddTodo"
+      variant="outline-primary"
+      onClick={props.onClick}
+    >
+      <Plus size={32} />
+      <span>Add</span>
+    </Button>
+  );
 };
 
 export default AddTodo;
