ef3a2654d14306dd83d2130b94c723f1b771f893
Huong Pham <huong.pq12@gmail.com>
2020-12-07 13:05:12 +0200

    feature: add dropdown to habit label

--- src/components/Label/Label.tsx
+++ src/components/Label/Label.tsx
@@ -1,16 +1,72 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
+import Dropdown from "react-bootstrap/Dropdown";
+import Form from "react-bootstrap/Form";
 
 interface IProps {
   labelType?: string;
+  habits?: string[];
+  addNewHabit: (habit: string) => void;
 }
 
-const Label: React.FunctionComponent<IProps> = ({ labelType }) => {
-  return (
-    <Button className={`component-label--${labelType ? labelType : ""}`}>
-      {labelType === "week" ? "Week goal" : "New habit"}
-    </Button>
-  );
-};
+interface IState {
+  label: string;
+  newHabit: string;
+}
+
+class Label extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      label: props.labelType === "week" ? "Week goal" : "New habit",
+      newHabit: "",
+    };
+  }
+
+  handleClick = (habit: string) => () => {
+    this.setState({ label: habit });
+  };
+
+  handleNewHabitChange = (e: any) => {
+    this.setState({ newHabit: e.target.value });
+  };
+
+  onSubmitNewHabit = (e: any) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      this.props.addNewHabit(this.state.newHabit);
+      this.setState({ newHabit: "" });
+    }
+  };
+
+  render() {
+    const { labelType, habits } = this.props;
+    return (
+      <Dropdown>
+        <Dropdown.Toggle
+          className={`component-label--${labelType ? labelType : ""}`}
+        >
+          {this.state.label}
+        </Dropdown.Toggle>
+        {labelType === "habit" && (
+          <Dropdown.Menu>
+            {habits!.map((habit, idx) => (
+              <Dropdown.Item key={idx} onClick={this.handleClick(habit)}>
+                {habit}
+              </Dropdown.Item>
+            ))}
+            <Form>
+              <Form.Control
+                value={this.state.newHabit}
+                placeholder="Enter a new habit"
+                onKeyPress={this.onSubmitNewHabit}
+                onChange={this.handleNewHabitChange}
+              />
+            </Form>
+          </Dropdown.Menu>
+        )}
+      </Dropdown>
+    );
+  }
+}
 
 export default Label;
